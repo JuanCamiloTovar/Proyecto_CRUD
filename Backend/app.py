1eb8f2e4c13dfeb343f120bb05b10ab58ef8bf4e
Juan Tovar <tovarcortesjuancamilo29@gmail.com>
2025-05-06 08:11:44 -0500

    Add files via upload

--- Backend/app.py
+++ Backend/app.py
@@ -15,7 +15,7 @@ from flasgger import Swagger
 
 app = Flask(__name__)
 CORS(app)
-swagger = Swagger(app)
+Swagger(app)
 
 # con el siguiente codigo se hace posible la conexion a la base de datos
 def conectar(vhost,vuser,vpass,vdb):
@@ -23,190 +23,184 @@ def conectar(vhost,vuser,vpass,vdb):
     return conn
 
 #Ruta para consulta general
-@app.route("/", methods=['GET'])
+@app.route('/', methods=['GET'])
 def consulta_general():
-    """
-    Consulta general del baul de contraseñas
-    ---
-    responses:
+  """
+  Consulta general del baul de contraseñas
+  ---
+  responses:
     200:
       descripcion: Lista de registros
-    """
-    try:
-        conn = conectar('localhost', 'root', '290307', 'gestor_contrasena')
-        cur = conn.cursor()
-        cur.execute("SELECT * FROM baul")
-        datos = cur.fetchall()
-        data = []
-        
-        for row in datos:
-            dato = {
-                'id_baul': row[0],
-                'Plataforma': row[1],
-                'usuario': row[2],
-                'clave': row[3]
-            }
-            data.append(dato)
-        cur.close()
-        conn.close()
-        return jsonify({'baul': data, 'mensaje': 'Baul de constraseñas'})
-    except Exception as ex:
-        print(ex)
-        return jsonify({'mensaje': 'Error'})
+  """
+  try:
+      conn = conectar('localhost', 'root', '290307', 'gestor_contrasena')
+      cur = conn.cursor()
+      cur.execute("SELECT * FROM baul")
+      datos = cur.fetchall()
+      data = []
+      
+      for row in datos:
+          dato = {
+              'id_baul': row[0],
+              'Plataforma': row[1],
+              'usuario': row[2],
+              'clave': row[3]
+          }
+          data.append(dato)
+      cur.close()
+      conn.close()
+      return jsonify({'baul': data, 'mensaje': 'Baul de constraseñas'})
+  except Exception as ex:
+      print(ex)
+      return jsonify({'mensaje': 'Error'})
 
 
 #Ruta para consulta individual
 @app.route('/consulta_individual/<codigo>', methods=['GET'])
 def consulta_individual(codigo):
-    """
-    Consulta individual por ID
-    ----
-    parameters:
-      - name: codigo
-        in: path
-        requried: true
-        type: integer
-    responses:
-      200:
-        description: Registro encontrado
-    """
-    try:
-        conn = conectar('localhost', 'root', '290307', 'gestor_contrasena')
-        cur = conn.cursor()
-        cur.execute(f"SELECT * FROM baul WHERE id_baul = '{codigo}'")
-        datos = cur.fetchone()
-        cur.close()
-        conn.close()
-        if datos:
-            dato = {'id_baul': datos[0], 'Plataforma': datos[1], 'usuario': datos[2], 'clave':datos[3]}
-            return jsonify({'baul': dato, 'mensaje': 'Resgistro encontado'})
-        else:
-            return jsonify({'mensaje': 'Registro no encontrado'})
-    except Exception as ex:
-        print(ex)
-        return jsonify({'mensaje': 'Error'})
+  """
+  Consulta individual por ID
+  ---
+  parameters:
+    - name: codigo
+      in: path
+      required: true
+      type: integer
+  responses:
+    200:
+      description: Registro encontrado
+  """
+  try:
+      conn = conectar('localhost', 'root', '290307', 'gestor_contrasena')
+      cur = conn.cursor()
+      cur.execute(f"SELECT * FROM baul WHERE id_baul = '{codigo}'")
+      datos = cur.fetchone()
+      cur.close()
+      conn.close()
+      if datos:
+          dato = {'id_baul': datos[0], 'Plataforma': datos[1], 'usuario': datos[2], 'clave':datos[3]}
+          return jsonify({'baul': dato, 'mensaje': 'Resgistro encontado'})
+      else:
+          return jsonify({'mensaje': 'Registro no encontrado'})
+  except Exception as ex:
+      print(ex)
+      return jsonify({'mensaje': 'Error'})
 
 # si el codigo no funciona colocar / al final
 
 @app.route('/registro/', methods=['POST'])
 def registro():
-    """
-    Registrar nueva contraseña
-    -----
-    parameters: 
-      - name: body
-        in: body
-        requeried: True
-        schema:
-          type: object
-          propierties:
-            plataforma:
-              type: string
-            usuario:
-              type: string
-            clave:
-              type: string
-    responses:
-        200:
-          description: Registro agregado
-    """
-    
-    try:
-        data = request.get_json()
-        plataforma = data['plataforma']
-        usuario = data['usuario']
-        clave = bcrypt.hashpw(data['clave'].encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
-        
-        conn = conectar('localhost', 'root', '290307', 'gestor_contrasena')
-        cur = conn.cursor()
-        cur.execute("INSERT INTO baul (plataforma, usuario, clave) VALUES (%s, %s, %s)",
-                    (plataforma,usuario,clave))
-        conn.commit()
-        cur.close()
-        conn.close()
-        return jsonify({'mensaje': 'Registro agregado'})
-        
-    except Exception as ex:
-        print(ex)
-        return jsonify({'mensaje': 'error'})
+  """
+  Registrar nueva contraseña
+  ---
+  parameters: 
+    - name: body
+      in: body
+      required: true
+      schema:
+        type: object
+        properties:
+          plataforma:
+            type: string
+          usuario:
+            type: string
+          clave:
+            type: string
+  responses:
+    200:
+      description: Registro agregado
+  """
+  try:
+      data = request.get_json()
+      plataforma = data['plataforma']
+      usuario = data['usuario']
+      clave = bcrypt.hashpw(data['clave'].encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+      
+      conn = conectar('localhost', 'root', '290307', 'gestor_contrasena')
+      cur = conn.cursor()
+      cur.execute("INSERT INTO baul (plataforma, usuario, clave) VALUES (%s, %s, %s)",
+                  (plataforma,usuario,clave))
+      conn.commit()
+      cur.close()
+      conn.close()
+      return jsonify({'mensaje': 'Registro agregado'})
+      
+  except Exception as ex:
+      print(ex)
+      return jsonify({'mensaje': 'error'})
 
 #Ruta para eliminar registro
 @app.route('/eliminar/<codigo>', methods=['DELETE'])
 def eliminar(codigo):
-
-    """
-    Eliminar registro por su ID
+# Si el codigo no funciona colocar depues de codigo una , linea 159
+  """
+    Eliminar registro por ID
     ---
     parameters:
       - name: codigo
         in: path
-        requeried: True
+        required: true
         type: integer
     responses:
       200:
-        description: Resgistro eliminado
+        description: Registro eliminado
     """
-
-# Si el codigo no funciona colocar depues de codigo una , linea 159
-
-    try:
-        conn = conectar('localhost', 'root', '290307', 'gestor_contrasena')
-        cur = conn.cursor()
-        cur.execute("DELETE FROM baul WHERE id_baul = %s", (codigo))
-        conn.commit()
-        cur.close()
-        conn.close()
-        return jsonify({'mensaje': 'Regristro Eliminado'})
-    except Exception as ex:
-        print(ex)
-        return jsonify({'mensaje': 'Error'})
-
-
+  try:
+      conn = conectar('localhost', 'root', '290307', 'gestor_contrasena')
+      cur = conn.cursor()
+      cur.execute("DELETE FROM baul WHERE id_baul = %s", (codigo))
+      conn.commit()
+      cur.close()
+      conn.close()
+      return jsonify({'mensaje': 'Regristro Eliminado'})
+  except Exception as ex:
+      print(ex)
+      return jsonify({'mensaje': 'Error'})
 
 # Ruta para actualizar un registro
 @app.route('/actualizar/<codigo>', methods=['PUT'])
 def actualizar(codigo):
-    """
+  """
     Actualizar registro por ID
-    ----          
+    ---
     parameters:
       - name: codigo
         in: path
-        requeried: true
+        required: true
         type: integer
       - name: body
         in: body
-        requeried: true
-        sschema:
+        required: true
+        schema:
           type: object
-          propierties:
+          properties:
             plataforma:
-                type: string
+              type: string
             usuario:
-                type: string
+              type: string
             clave:
-                type: string
+              type: string
     responses:
       200:
-        description: Registro Actualizado
+        description: Registro actualizado
     """
-    try:
-        data = request.get_json()
-        plataforma = data['plataforma']
-        usuario = data['usuario']
-        clave = bcrypt.hashpw(data['clave'].encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+  try:
+      data = request.get_json()
+      plataforma = data['plataforma']
+      usuario = data['usuario']
+      clave = bcrypt.hashpw(data['clave'].encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
 
-        conn = conectar('localhost', 'root', '290307', 'gestor_contrasena')
-        cur = conn.cursor()
-        cur.execute("UPDATE baul SET plataforma = %s, usuario = %s, clave = %s WHERE id_baul = %s",
-                    (plataforma, usuario, clave, codigo))
-        conn.commit()
-        cur.close()
-        conn.close()
-        return jsonify({'mensaje': 'Registro actualizado'})
-    except Exception as ex:
-        print(ex)
-        return jsonify({'mensaje': 'Error'})
-        
+      conn = conectar('localhost', 'root', '290307', 'gestor_contrasena')
+      cur = conn.cursor()
+      cur.execute("UPDATE baul SET plataforma = %s, usuario = %s, clave = %s WHERE id_baul = %s",
+                  (plataforma, usuario, clave, codigo))
+      conn.commit()
+      cur.close()
+      conn.close()
+      return jsonify({'mensaje': 'Registro actualizado'})
+  except Exception as ex:
+      print(ex)
+      return jsonify({'mensaje': 'Error'})
+
 if __name__=='__main__':
     app.run(debug=True)
